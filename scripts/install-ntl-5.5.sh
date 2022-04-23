109c532f5fdb6c3c5495a131549aeccebc356f0f
Ning Shang <syncom.dev@gmail.com>
2022-04-23 12:15:00 -0700

    Add release workflow
    Add a workflow to publish release tarballs through Git tags.
    
    The tags must follow the SEMVER format, and must match the version
    specified in `AC_INIT` in "configure.ac".
    
    We also refactored files in the "scripts/" directory for this to work.

--- scripts/install-ntl-5.5.sh
+++ scripts/install-ntl-5.5.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+INSTALL_DIR="$1"
+
 wget -c https://libntl.org/ntl-5.5.2.tar.gz -O /tmp/ntl.tar.gz
 tar xvfz /tmp/ntl.tar.gz -C /tmp/
 cd /tmp/ntl-5.5.2/src
-./configure PREFIX=/tmp/nssw
+./configure PREFIX="${INSTALL_DIR}"
 make
 make install

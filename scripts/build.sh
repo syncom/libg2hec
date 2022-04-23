b5689cf5beba46a8123cf84dcc0dd8f150464130
Ning Shang <syncom.dev@gmail.com>
2022-04-23 12:51:24 -0700

    GHA workflow improvements
    Add a `distcheck` argument to "script/build.sh".
    Update "README.md".
    Use ubuntu-20.04 runners.

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,16 +6,22 @@ SCRIPT_DIR=$(dirname "$0")
 FLAGS_TMPDIR=$(mktemp -d)
 readonly FLAGS_TMPDIR
 
+INSTALL_DIR_PREFIX=${INSTALL_DIR_PREFIX:-${FLAGS_TMPDIR}}
+
 # Where we run ./configure and NTL installation script
 cd "${SCRIPT_DIR}/.."
 
 # Install NTL
-scripts/install-ntl-5.5.sh "${FLAGS_TMPDIR}"
+scripts/install-ntl-5.5.sh "${INSTALL_DIR_PREFIX}"
 
-env LDFLAGS="-L${FLAGS_TMPDIR}/lib" \
-    CXXFLAGS="-I${FLAGS_TMPDIR}/include" \
+env LDFLAGS="-L${INSTALL_DIR_PREFIX}/lib" \
+    CXXFLAGS="-I${INSTALL_DIR_PREFIX}/include" \
     ./configure \
-    --prefix="${FLAGS_TMPDIR}"
+    --prefix="${INSTALL_DIR_PREFIX}"
 make
-make install
-DISTCHECK_CONFIGURE_FLAGS="LDFLAGS=-L${FLAGS_TMPDIR}/lib CXXFLAGS=-I${FLAGS_TMPDIR}/include" make distcheck
+
+if [ "$#" -ge 1 ] && [ "$1" = "distcheck" ]; then
+  echo "Creating tarball for distribution"
+  make install
+  DISTCHECK_CONFIGURE_FLAGS="LDFLAGS=-L${INSTALL_DIR_PREFIX}/lib CXXFLAGS=-I${INSTALL_DIR_PREFIX}/include" make distcheck
+fi

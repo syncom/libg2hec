745c20d2dc4316e1501cb619d9e4cec2256ca312
Ning Shang <syncom.dev@gmail.com>
2022-04-23 12:15:00 -0700

    Add release workflow
    Add a workflow to publish release tarballs through Git tags.
    
    The tags must follow the SEMVER format, and must match the version
    specified in `AC_INIT` in "configure.ac".
    
    We also refactored files in the "scripts/" directory for this to work.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,19 @@
 set -euxo pipefail
 
 SCRIPT_DIR=$(dirname "$0")
+FLAGS_TMPDIR=$(mktemp -d)
+readonly FLAGS_TMPDIR
 
-# Where we run ./configure
+# Where we run ./configure and NTL installation script
 cd "${SCRIPT_DIR}/.."
 
-env LDFLAGS=-L/tmp/nssw/lib CXXFLAGS=-I/tmp/nssw/include ./configure \
-    --prefix=/tmp/nssw
+# Install NTL
+scripts/install-ntl-5.5.sh "${FLAGS_TMPDIR}"
+
+env LDFLAGS="-L${FLAGS_TMPDIR}/lib" \
+    CXXFLAGS="-I${FLAGS_TMPDIR}/include" \
+    ./configure \
+    --prefix="${FLAGS_TMPDIR}"
 make
-make check
+make install
+DISTCHECK_CONFIGURE_FLAGS="LDFLAGS=-L${FLAGS_TMPDIR}/lib CXXFLAGS=-I${FLAGS_TMPDIR}/include" make distcheck

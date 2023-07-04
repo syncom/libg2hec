eaefd2529e90d82433b872d5e8045c94b9f45b6e
Ning Shang <syncom.dev@gmail.com>
2023-07-04 12:03:55 -0700

    Reformat sa-check.sh for better readability
    A minor refactor to improve readability.

--- scripts/sa-check.sh
+++ scripts/sa-check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#
 # bear and clang-check (included in LLVM tools) needs to be installed
 # for static analysis
 
@@ -33,13 +33,15 @@ else
 fi
 while read -r FILE; do
   cd "$(dirname "${FILE}")"
-  output=$(clang-check -analyze -extra-arg -Xanalyzer -extra-arg \
-    -analyzer-disable-checker=unix \
-    -extra-arg -Xanalyzer -extra-arg \
-    -analyzer-checker=core,deadcode,security,cplusplus,nullability \
-    -extra-arg -Xanalyzer -extra-arg \
-    -analyzer-disable-checker=security.insecureAPI.DeprecatedOrUnsafeBufferHandling \
-    -extra-arg -Xanalyzer -extra-arg -analyzer-output=text \
+  output=$(clang-check -analyze \
+    -extra-arg -Xanalyzer \
+    -extra-arg -analyzer-disable-checker=unix \
+    -extra-arg -Xanalyzer \
+    -extra-arg -analyzer-checker=core,deadcode,security,cplusplus,nullability \
+    -extra-arg -Xanalyzer \
+    -extra-arg -analyzer-disable-checker=security.insecureAPI.DeprecatedOrUnsafeBufferHandling \
+    -extra-arg -Xanalyzer \
+    -extra-arg -analyzer-output=text \
     -p "${COMPILE_DB_DIR}" \
     "$(basename "${FILE}")" 2>&1)
   if grep -q "warning:" <<< "${output}"; then
